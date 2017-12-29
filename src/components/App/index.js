2cde4247264fcfaf0d5723ee127618903e62fde9
Kiarash Bahmani <bahmani.kiarash@gmail.com>
2017-12-28 19:55:53 -0800

    added c3js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,15 +3,23 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Loadable from 'react-loadable'
 import AsyncLoading from '../common/AsyncLoading.js'
+import Chart from 'react-c3js';
+import 'c3/c3.css';
 import './style.css'
 
 class App extends Component {
 
 
   render() {
+    const data = {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    };
     return (
       <div className='app'>
-        APP 2
+        <Chart data={data} />
       </div>
     )
   }
